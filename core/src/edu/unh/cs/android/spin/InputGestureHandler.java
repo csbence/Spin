57961861c44591530dbdb5a8facbd23342f99096
Bence Cserna <bence@cserna.net>
2015-04-07 16:52:47 -0400

    Gesture action handler added.

---
+++ core/src/edu/unh/cs/android/spin/InputGestureHandler.java
@@ -0,0 +1,62 @@
+package edu.unh.cs.android.spin;
+
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Queue;
+
+import edu.unh.cs.android.spin.action.Action;
+import edu.unh.cs.android.spin.action.ActionThrow;
+
+public class InputGestureHandler implements GestureDetector.GestureListener {
+    private final Queue<Action> actionQueue;
+
+    public InputGestureHandler(Queue<Action> actionQueue) {
+        this.actionQueue = actionQueue;
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        Vector2 vector2 = new Vector2(velocityX, velocityY);
+
+        final ActionThrow actionThrow = new ActionThrow(vector2.angle());
+        actionQueue.offer(actionThrow);
+
+        return true;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+}
--- core/src/edu/unh/cs/android/spin/MyGdxGame.java
+++ core/src/edu/unh/cs/android/spin/MyGdxGame.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.input.GestureDetector;
 
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -14,42 +14,55 @@ import java.util.concurrent.LinkedBlockingQueue;
 import edu.unh.cs.android.spin.action.Action;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-	private final Queue<Action> actionQueue = new LinkedBlockingQueue<>();
+    private SpriteBatch batch;
+    private Texture img;
+    private final Queue<Action> actionQueue = new LinkedBlockingQueue<>();
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("magic_ball_64.png");
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("magic_ball_64.png");
 
-		InputProcessor inputProcessor = new InputEventHandler(actionQueue);
-		Gdx.input.setInputProcessor(inputProcessor);
-	}
+        final InputProcessor inputProcessor = new GestureDetector(new InputGestureHandler(actionQueue));
+        Gdx.input.setInputProcessor(inputProcessor);
 
-	@Override
-	public void resize (int width, int height) {
-	}
+        // TODO Create an object store to keep track of the object on the field (e.g. flying balls)
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0.9f, 0.9f, 0.9f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    @Override
+    public void resize(int width, int height) {
+    }
 
-		batch.begin();
-		batch.draw(img, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		batch.end();
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.9f, 0.9f, 0.9f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-	@Override
-	public void pause () {
-	}
+        final Action action = actionQueue.poll();
 
-	@Override
-	public void resume () {
-	}
+        if (action != null) {
+            // TODO: Execute action
+            // Create a new ball and add itt to the object store
+            // Set the balls direction and starting location.
+        }
 
-	@Override
-	public void dispose () {
-	}
+        batch.begin();
+
+        // TODO move the balls on the field
+
+        batch.draw(img, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        batch.end();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
